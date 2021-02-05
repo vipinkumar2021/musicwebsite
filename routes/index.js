4884e774d778adc9a8b27e512fc20aacbcad4ddc
Vipin Kumar <resetpa7@gmail.com>
2021-02-05 16:36:31 +0530

    added header, footer, modal.ejs

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SaReGaMa Music Academy & GMP Studio' });
+  res.render('index', { title: 'SaReGaMa Music Academy & GMP Studio', msg: '' });
 });
 
 module.exports = router;
